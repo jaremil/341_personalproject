b1a0d40e2e357ce5d5abf72ddc00883df8133fd0
Jade Miller <mil19017@byui.edu>
2025-05-31 22:37:45 -0600

    bug fix?

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.static('public'));
 app.use('/', indexRouter);
 
 const mongoose = require('mongoose');
-mongoose.connect(process.env.DATABASE_URL)
+mongoose.connect('process.env.DATABASE_URL')
 
 const db = mongoose.connection;
 db.on('error', error => console.error(error));
